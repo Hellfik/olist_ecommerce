fd25178cda7a33acf148e810eaad67aac243e058
Fayeulle <mika.fayeulle@gmail.com>
2021-03-07 11:51:24 +0100

    Database fill with data + indicators queries

---
+++ olist_fill_tables.py
@@ -0,0 +1,159 @@
+import sqlite3
+import pandas as pd
+
+DB_NAME = 'olist_db.db'
+
+con = sqlite3.connect(DB_NAME)
+cur = con.cursor()
+cur.execute('pragma encoding=UTF8')
+
+"""
+CUSTOMERS DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+customers_dataset = pd.read_csv('datasets/olist_customers_dataset.csv')
+
+customers_df = pd.DataFrame(customers_dataset)
+
+customers_dataset.drop_duplicates(subset=['customer_unique_id']).to_sql(
+    name="olist_customers_dataset",
+    con=con,
+    index=False,
+    if_exists='replace',
+    chunksize=500
+)
+
+
+"""
+GEOLOCATION DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+geolocation_dataset = pd.read_csv('datasets/olist_geolocation_dataset.csv')
+
+geolocation_df = pd.DataFrame(geolocation_dataset)
+
+geolocation_dataset.drop_duplicates(subset=['geolocation_zip_code_prefix']).to_sql(
+    name="olist_geolocation_dataset",
+    con=con,
+    index=False,
+    if_exists='replace',
+    chunksize=500
+)
+
+"""
+BASKET DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+order_list_items_dataset = pd.read_csv('datasets/olist_order_items_dataset.csv')
+
+order_list_items_df = pd.DataFrame(order_list_items_dataset)
+
+
+order_list_items_df.drop_duplicates(subset=['order_id']).to_sql(
+    name="olist_order_items_dataset",
+    con=con,
+    index=False,
+    if_exists='replace',
+    chunksize=500
+)
+
+"""
+ORDER PAYMENTS DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+order_payments_dataset = pd.read_csv('datasets/olist_order_payments_dataset.csv')
+order_payments_df = pd.DataFrame(order_payments_dataset)
+
+order_payments_df.to_sql(
+    name="olist_order_payments_dataset",
+    con=con,
+    index=True,
+    if_exists='replace',
+    chunksize=500
+)
+
+"""
+REVIEWS DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+order_reviews_dataset = pd.read_csv('datasets/olist_order_reviews_dataset.csv')
+order_reviews_df = pd.DataFrame(order_reviews_dataset)
+
+order_reviews_df.drop_duplicates(subset=['review_id']).to_sql(
+    name="olist_order_reviews_dataset",
+    con=con,
+    index=False,
+    if_exists='replace',
+    chunksize=500
+)
+
+"""
+ORDERS DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+orders_dataset = pd.read_csv('datasets/olist_orders_dataset.csv')
+order_df = pd.DataFrame(orders_dataset)
+
+order_df.to_sql(
+    name="olist_orders_dataset",
+    con=con,
+    index=False,
+    if_exists='replace',
+    chunksize=500
+)
+
+"""
+PRODUCTS DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+products_dataset = pd.read_csv('datasets/olist_products_dataset.csv')
+products_df = pd.DataFrame(products_dataset)
+
+products_df.to_sql(
+    name="olist_products_dataset",
+    con=con,
+    index=False,
+    if_exists='replace',
+    chunksize=500
+)
+
+"""
+SELLERS DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+sellers_dataset = pd.read_csv('datasets/olist_sellers_dataset.csv')
+sellers_df = pd.DataFrame(sellers_dataset)
+
+sellers_df.to_sql(
+    name="olist_sellers_dataset",
+    con=con,
+    index=False,
+    if_exists='replace',
+    chunksize=500
+)
+
+"""
+PRODUCT NAME CATEGORY DATAFRAME + WRITE INTO SQLITE TABLE
+
+"""
+
+product_name_dataset = pd.read_csv('datasets/product_category_name_translation.csv')
+product_name_df = pd.DataFrame(product_name_dataset)
+
+product_name_df.to_sql(
+    name="product_category_name_translation",
+    con=con,
+    index=False,
+    if_exists='replace',
+    chunksize=500
+)
+
+
---
+++ olist_tables.py
@@ -0,0 +1,164 @@
+import sqlite3
+
+
+DB_NAME = 'olist_db.db'
+
+con = sqlite3.connect(DB_NAME)
+cur = con.cursor()
+cur.execute('pragma encoding=UTF8')
+
+
+# Initialisation du dictionnaire de tables
+
+TABLES = {}
+
+TABLES['olist_customers_dataset'] = (
+    '''CREATE TABLE olist_customers_dataset (
+        customer_id char(32) NOT NULL,
+        customer_unique_id char(32),
+        customer_zip_code_prefix char(5),
+        customer_city char(60),
+        customer_state char(2),
+        PRIMARY KEY (customer_id),
+        FOREIGN KEY (customer_zip_code_prefix)
+            REFERENCES olist_geolocation_dataset(geolocation_zip_code_prefix)
+            ON DELETE SET NULL
+            ON UPDATE CASCADE
+        )''' )
+
+TABLES['olist_orders_dataset'] = (
+    '''CREATE TABLE olist_orders_dataset (
+        order_id char(32) NOT NULL,
+        customer_id char(32),
+        order_status varchar(20),
+        order_purchase_timestamp datetime,
+        order_approved_at datetime,
+        order_delivered_carrier_date datetime,
+        order_estimated_customer_date datetime,
+        order_estimated_delivery_date datetime,
+        PRIMARY KEY (order_id),
+        FOREIGN KEY (customer_id)
+            REFERENCES customers_dataset(customer_id)
+            ON DELETE SET NULL
+            ON UPDATE CASCADE
+        )''' )
+
+TABLES['olist_geolocation_dataset'] = (
+    '''CREATE TABLE olist_geolocation_dataset (
+        geolocation_zip_code_prefix mediumint NOT NULL,
+        geolocation_lat decimal(16,14),
+        geolocation_lng decimal(16,14),
+        geolocation_city varchar(30),
+        geolocation_state char(2)
+        )''' )
+
+TABLES['olist_order_items_dataset'] = (
+    '''CREATE TABLE olist_order_items_dataset (
+        order_id char(32) NOT NULL,
+        order_item_id char(32) NOT NULL,
+        product_id char(32) NOT NULL,
+        seller_id char(32) NOT NULL,
+        shipping_limit_date datetime,
+        price DECIMAL(9,2) NOT NULL,
+        freight_value DECIMAL (8,2),
+        PRIMARY KEY (order_id),
+        FOREIGN KEY (order_id)
+            REFERENCES olist_orders_dataset(order_id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+        FOREIGN KEY (product_id)
+            REFERENCES olist_products_dataset(product_id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+        FOREIGN KEY (seller_id)
+            REFERENCES olist_sellers_dataset(seller_id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+        )''' )
+
+TABLES['olist_order_payments_dataset'] = (
+    '''CREATE TABLE olist_order_payments_dataset (
+        id bigint auto_increment not null,
+        order_id char(32),
+        payment_sequential tinyint not null,
+        payment_type varchar(20) not null,
+        payment_installments tinyint not null,
+        payment_value decimal(9,2) not null,
+        PRIMARY KEY (id),
+        FOREIGN KEY (order_id)
+            REFERENCES olist_order_dataset(order_id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+        )''' )
+
+TABLES['olist_order_reviews_dataset'] = (
+    '''CREATE TABLE olist_order_reviews_dataset (
+        review_id char(32),
+        order_id char(32),
+        review_score tinyint,
+        review_comment_title varchar(50),
+        review_comment_message varchar(250),
+        review_creation_date datetime,
+        review_answer_timestamp datetime
+        )''' )
+
+TABLES['olist_products_dataset'] = (
+    '''CREATE TABLE olist_products_dataset (
+        product_id char(32),
+        product_category_name varchar(30),
+        product_name_length tinyint,
+        product_description_length smallint,
+        product_photo_qty tinyint,
+        product_weight_g smallint,
+        product_length_cm tinyint,
+        product_height_cm tinyint,
+        product_width_cm tinyint,
+        PRIMARY KEY (product_id),
+        FOREIGN KEY (product_category_name)
+            REFERENCES product_category_name_translation(product_category_name_english)
+            ON DELETE SET NULL
+            ON UPDATE CASCADE
+        )''' )
+
+TABLES['olist_sellers_dataset'] = (
+    '''CREATE TABLE olist_sellers_dataset (
+        seller_id char(32),
+        seller_zip_code_prefix char(5),
+        seller_city varchar(30),
+        seller_state char(2),
+        PRIMARY KEY (seller_id),
+        FOREIGN KEY (seller_zip_code_prefix)
+            REFERENCES olist_geolocation_dataset(geolocation_zip_code_prefix)
+            ON DELETE SET NULL
+            ON UPDATE CASCADE
+        )''' )
+    
+TABLES['product_category_name_translation'] = (
+    '''CREATE TABLE product_category_name_translation (
+        product_category_name varchar(30),
+        product_category_name_english varchar(30),
+        PRIMARY KEY (product_category_name)
+        )''' )
+
+
+
+# Créer les tables du dictionnaire TABLES
+
+for table_name in TABLES:
+    table_description = TABLES[table_name]
+    print("Creating table {}: \n".format(table_name), end='')
+    cur.execute(table_description)
+
+
+
+
+
+# Save (commit) the changes
+con.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+con.close()
+
+
+
